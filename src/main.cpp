463c4ba461fa14d7bcb3cb401a5caec739a345ef
Timothy Fish <timothyfish@vt.edu>
2025-05-01 14:49:37 -0400

    changed GPIO pins

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <BluetoothSerial.h>
 #include <PS4Controller.h>
 
-#define ESC1_PIN 26
-#define ESC2_PIN 27
+#define ESC1_PIN 32
+#define ESC2_PIN 22
 #define ESC1_CH 0
 #define ESC2_CH 1
 #define FREQ 50
